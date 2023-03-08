e0e615210749b59f80d43afafa5a0821df9a350c
sadrayavar <sadra.yavarzadeh@gmail.com>
2023-03-09 00:30:15 +0330

    add basic remove and edit buttons for entries

--- ui.js
+++ ui.js
@@ -9,15 +9,37 @@ export default class Ui {
 	}
 	renderEvent(name, date, type) {
 		const container = document.createElement("div")
-
+		container.id = name
 		;[name, date, type].forEach((data) => {
 			const element = document.createElement("div")
 			element.innerHTML = data
-			container.append(data)
-			container.append(document.createElement("br"))
+			container.append(element)
 		})
+		;["edit", "remove"].forEach((entry) => {
+			const element = document.createElement("button")
+			element.innerHTML = entry
+
+			if (entry == "edit") {
+				element.onclick = this.editEntry
+			} else {
+				element.onclick = this.removeEntry
+			}
+
+			container.append(element)
+		})
+		container.append(document.createElement("br"))
 		container.append(document.createElement("br"))
 
 		return container
+	}
+	editEntry(event) {
+		const id = event.target.parentNode.id
+
+		alert(`${id} removed`)
+	}
+	removeEntry(event) {
+		const id = event.target.parentNode.id
+
+		alert(`${id} removed`)
 	}
 }
